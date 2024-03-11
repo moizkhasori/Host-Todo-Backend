7f84a680804109217c69f3936e7c3b4668d3321f
moizkhasori <140612953+moizkhasori@users.noreply.github.com>
2024-03-11 15:31:45 +0500

    samesite-none | Update CookieAndToken.js

--- Utils/CookieAndToken.js
+++ Utils/CookieAndToken.js
@@ -2,7 +2,11 @@ export const generateTokenAndSendCookie = (user, statusCode, res) => {
 
     const token = user.getJwtToken();
 
-    res.status(statusCode).cookie("token",token).json({
+    res.status(statusCode).cookie("token",token,{
+        // Other cookie options...
+        sameSite: 'None',
+        secure: true // Required when using SameSite=None
+    }).json({
         success:true,
         user
     })
