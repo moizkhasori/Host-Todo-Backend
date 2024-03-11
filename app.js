667a85fbbcffb276602e62d11a4a61a498c5632b
moizkhasori <140612953+moizkhasori@users.noreply.github.com>
2024-03-11 14:00:48 +0500

    corsOrigin - Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials:true
-}))
+app.use(cors())
 
 // routes import
 import userRouter from "./Routes/Users/userRoute.js"
