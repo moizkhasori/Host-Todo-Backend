474f392bb06bc8b57a0b2ab73288e19aaa87f43a
moizkhasori <140612953+moizkhasori@users.noreply.github.com>
2024-03-11 14:38:33 +0500

    frontend url mistake correct - Update app.js

--- app.js
+++ app.js
@@ -8,10 +8,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors(
-  app.use(cors({
     origin: 'https://main--fabulous-paletas-de85bb.netlify.app/',
     credentials:true
-}))
 ))
 
 // routes import
